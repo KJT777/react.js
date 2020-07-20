04558c66f282a2a0c0551e731a22f6636fe2e886
Kasia Trzebiatowski <trzebiatowska@vp.pl>
2020-07-21 00:29:49 +0200

    add new files

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,65 +1,15 @@
-import React from 'react';// import biblioteki React
+import React from 'react';
 import styles from './List.scss';
-import Hero from '../Hero/Hero.js';
-import PropTypes from 'prop-types';
-import Column from '../Column/Column.js';
-import {settings} from '../../data/dataStore';
-import ReactHtmlParser from 'react-html-parser';
-import Creator from '../Creator/Creator.js';
-
 
 class List extends React.Component {
-  static propTypes = {
-    title: PropTypes.node.isRequired,
-    description: PropTypes.node,
-    columns: PropTypes.array,
-    image: PropTypes.node,
-  }
-
-  static defaultProps = {
-    description: settings.defaultListDescription,
-  }
-
-  state = {
-    columns: this.props.columns || [],
-  }
-
-  addColumn(title){
-    this.setState(state => (
-      {
-        columns: [
-          ...state.columns,
-          {
-            key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
-            title,
-            icon: 'list-alt',
-            cards: []
-          }
-        ]
-      }
-    ));
-  }
-
   render() {
-    const { title, image, description, columns, addColumn } = this.props;
     return (
       <section className={styles.component}>
-        <Hero titleText={this.props.title} image={this.props.image}/>
-        <div className={styles.description}>{ReactHtmlParser(description)}</div>
-        <div className={styles.columns}>
-        {/*<Column titleColumn={'Animals'} />
-          <Column titleColumn={'Plants'} />
-          <Column titleColumn={'Minerals'} />*/}
-        {this.state.columns.map(({key, ...columnProps}) => (
-        <Column key={key} {...columnProps} />
-        ))}
-        </div>
-        <div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
-        </div>
+       <h1 className={styles.title}>My first React app</h1>
+       <h2 className={styles.subtitle}>Hello world!</h2>
       </section>
-    );
+    )
   }
 }
 
-export default List;// export klasy List
+export default List;
